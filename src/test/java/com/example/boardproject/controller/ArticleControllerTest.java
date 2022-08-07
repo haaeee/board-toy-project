7734c978db3e6c2b2427d1f731e1ecaf11227629
jmjmjames <chungjm0711@gmail.com>
2022-08-07 14:39:41 +0900

    #16 - 컨트롤러 테스트 업데이트
    미디어 타입 검사 방법을 a433868 에서 확인했기 때문에, 나머지 뷰도 동일하게 작동할 것으로 예상하고 
    미리 테스트 메소드 업데이트

--- src/test/java/com/example/boardproject/controller/ArticleControllerTest.java
+++ src/test/java/com/example/boardproject/controller/ArticleControllerTest.java
@@ -23,7 +23,7 @@ class ArticleControllerTest {
     public ArticleControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
     }
-    
+
     @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
     @Test
     void givenNothing_whenRequestingArticlesView_thenReturnArticlesView() throws Exception {
@@ -46,7 +46,7 @@ class ArticleControllerTest {
         // when & then
         mvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
                 .andExpect(model().attributeExists("article"))
                 .andExpect(model().attributeExists("articleComments"));
@@ -61,7 +61,7 @@ class ArticleControllerTest {
         // when & then
         mvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/search"));
     }
 
@@ -74,7 +74,7 @@ class ArticleControllerTest {
         // when & then
         mvc.perform(get("/articles/search-hashtag"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/hashtag"));;
     }
 }
