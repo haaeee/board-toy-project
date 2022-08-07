bbf7950034039ff6d50f7fd5e305865b1f5d77a0
jmjmjames <chungjm0711@gmail.com>
2022-08-07 13:52:35 +0900

    #16 - 게시판 뷰 기본 골격 작성
    컨트롤러 핸들러 메소드를 테스트 통과하게 잡음
    뷰를 적절히 뼈대를 잡음
    테스트에서는 media type검사에서 부가적인 옵션이 들어갈 수 없음을 확인했기에 적절하게 수정함

--- src/main/java/com/example/boardproject/controller/ArticleController.java
+++ src/main/java/com/example/boardproject/controller/ArticleController.java
@@ -1,10 +1,19 @@
 package com.example.boardproject.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @RequestMapping("/articles")
 @Controller
 public class ArticleController {
 
+    @GetMapping
+    public String articles(Model model) {
+        model.addAttribute("articles", List.of());
+        return "articles/index";
+    }
 }
