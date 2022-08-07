f1e84106c0ddabaf0737fa80a4ac35ed7e04073d
jmjmjames <chungjm0711@gmail.com>
2022-08-07 17:39:29 +0900

    #16 - 게시글 상세 페이지 기본 골격 구성
    컨트롤러 뷰 테스트까지 한번에 적용

--- src/main/java/com/example/boardproject/controller/ArticleController.java
+++ src/main/java/com/example/boardproject/controller/ArticleController.java
@@ -3,6 +3,7 @@ package com.example.boardproject.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -15,5 +16,12 @@ public class ArticleController {
     public String articles(Model model) {
         model.addAttribute("articles", List.of());
         return "articles/index";
+    }
+
+    @GetMapping("/{articleId}")
+    public String articles(@PathVariable Long articleId, Model model) {
+        model.addAttribute("article", "article");  // TODO: 구현할 때 여기에 실제 데이터 넣어야함
+        model.addAttribute("articleComments", List.of());
+        return "articles/detail";
     }
 }
