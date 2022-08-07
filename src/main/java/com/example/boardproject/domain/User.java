818cc9210aef39fbed4319f7a6ba7cc1bf27b9ed
jmjmjames <chungjm0711@gmail.com>
2022-08-07 19:14:06 +0900

    #18 - 회원 계정 도메인 구현 및 erd 반영
    `Users` 이름으로 회원 계정 도메인 생성
    `user`, `account` 등은 mysql 예약어이므로 피함
    필드명도 mysql 예약어를 의식하여 정함
    
    테스트 데이터에 임의 계정 1개 등록
    테스트용이지만 패스워드가 노출되는 방식이므로 고민 필요
    
    회원 정보는 API 로 노출되지 않았으면 한다.
    이에 테스트에 404 not found 를 확인하는 테스트 추가
    간단하게 모든 http 메소드에 대응

---
+++ src/main/java/com/example/boardproject/domain/User.java
@@ -0,0 +1,66 @@
+package com.example.boardproject.domain;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Getter
+@ToString
+@Table(name = "users", indexes = {
+        @Index(columnList = "user_id"),
+        @Index(columnList = "email", unique = true),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@Entity
+public class User extends AuditingFields {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Setter
+    @Column(nullable = false, length = 100)
+    private String email;
+
+    @Setter
+    @Column(nullable = false)
+    private String userPassword;
+
+    @Setter
+    @Column(length = 100)
+    private String nickname;
+
+    @Setter
+    private String memo;
+
+    protected User() {
+    }
+
+    private User(String email, String userPassword, String nickname, String memo) {
+        this.email = email;
+        this.userPassword = userPassword;
+        this.nickname = nickname;
+        this.memo = memo;
+    }
+
+    public static User of(String email, String userPassword, String nickname, String memo) {
+        return new User(email, userPassword, nickname, memo);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User user)) return false;
+        return id != null && id.equals(user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
