1c24d49f57a92728fb561932de00d4e26373568e
jmjmjames <chungjm0711@gmail.com>
2022-08-10 00:40:25 +0900

    #24 - 서비스 로직 테스트 코드 업데이트 및 DTO 재설계, 쿼리 메소드 작성 테스트 코드를 작성하고 연관된 DTO 를 재설계함 테스트가 필요로 하는 repository 쿼리 메소드도 작성
    댓글 Repo 에 존재하는
    List<ArticleComment> findByArticle_Id(Long articleId);
    Article 테이블에서 id를 조회 ((_) == `의`)

--- src/main/java/com/example/boardproject/dto/ArticleDto.java
+++ src/main/java/com/example/boardproject/dto/ArticleDto.java
@@ -1,20 +1,54 @@
 package com.example.boardproject.dto;
 
+import com.example.boardproject.domain.Article;
+
 import java.time.LocalDateTime;
 
 public record ArticleDto(
-        LocalDateTime createdAt,
-        String createdBy,
+        Long id,
+        UserDto userDto,
         String title,
         String content,
-        String hashtag
+        String hashtag,
+        LocalDateTime createdAt,
+        String createdBy,
+        LocalDateTime modifiedAt,
+        String modifiedBy
 ) {
-    public static ArticleDto of(LocalDateTime createdAt,
-                                String createdBy,
+    public static ArticleDto of(Long id,
+                                UserDto userDto,
                                 String title,
                                 String content,
-                                String hashtag) {
-        return new ArticleDto(createdAt, createdBy, title, content, hashtag);
+                                String hashtag,
+                                LocalDateTime createdAt,
+                                String createdBy,
+                                LocalDateTime modifiedAt,
+                                String modifiedBy) {
+        return new ArticleDto(id, userDto, title, content, hashtag, createdAt, createdBy, modifiedAt, modifiedBy);
+    }
+
+    // 위 아래의 2가지 method 를 통해 dto -> domain 영향을 없앰
+    public static ArticleDto from(Article entity) {
+        return new ArticleDto(
+                entity.getId(),
+                UserDto.from(entity.getUser()),
+                entity.getTitle(),
+                entity.getContent(),
+                entity.getHashtag(),
+                entity.getCreatedAt(),
+                entity.getCreatedBy(),
+                entity.getModifiedAt(),
+                entity.getModifiedBy()
+        );
+    }
+
+    public Article toEntity() {
+        return Article.of(
+                userDto.toEntity(),
+                title,
+                content,
+                hashtag
+        );
     }
 
 }
--- src/main/java/com/example/boardproject/service/ArticleService.java
+++ src/main/java/com/example/boardproject/service/ArticleService.java
@@ -2,10 +2,11 @@ package com.example.boardproject.service;
 
 import com.example.boardproject.domain.type.SearchType;
 import com.example.boardproject.dto.ArticleDto;
-import com.example.boardproject.dto.ArticleUpdateDto;
+import com.example.boardproject.dto.ArticleWithCommentsDto;
 import com.example.boardproject.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,21 +18,22 @@ public class ArticleService {
     private final ArticleRepository articleRepository;
 
     @Transactional(readOnly = true)
-    public Page<ArticleDto> searchArticles(SearchType title, String searchKeyword) {
+    public Page<ArticleDto> searchArticles(SearchType searchType, String searchKeyword, Pageable pageable) {
         return Page.empty();
     }
 
     @Transactional(readOnly = true)
-    public ArticleDto searchArticle(long articleId) {
+    public ArticleWithCommentsDto getArticle(long articleId) {
         return null;
     }
 
     public void saveArticle(ArticleDto dto) {
     }
 
-    public void updateArticle(long articleId, ArticleUpdateDto dto) {
+    public void updateArticle(ArticleDto dto) {
     }
 
     public void deleteArticle(long articleId) {
     }
+
 }
