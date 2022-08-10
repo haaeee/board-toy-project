984de2ad88760a4363dc91bedf535ac3cb1ff040
jmjmjames <chungjm0711@gmail.com>
2022-08-11 03:13:53 +0900

    #30 - 페이지네이션 기능을 컨트롤러(게시판 뷰)에 반영 테스트는 `Page`클래스를 이용하기 때문에 정렬을 함께 테스트 정렬 구현해서 이어서 테스트 코드 작성해야함

--- src/test/java/com/example/boardproject/controller/ArticleControllerTest.java
+++ src/test/java/com/example/boardproject/controller/ArticleControllerTest.java
@@ -4,6 +4,7 @@ import com.example.boardproject.config.SecurityConfig;
 import com.example.boardproject.dto.ArticleWithCommentsDto;
 import com.example.boardproject.dto.UserDto;
 import com.example.boardproject.service.ArticleService;
+import com.example.boardproject.service.PaginationService;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,17 +13,27 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Set;
 
 import static org.mockito.BDDMockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+/**
+ * 42 : MockBean annotation의 Target은 Type, Field 생성자 주입 안됨
+ * 48 : 슬라이스 테스트에서는 생성자를 통한 주입이여도 @Autowired 해줘야함(WebMvcTest에 ExtendWith가 생성자 주입을 도움)
+ * 즉, spring 이 생성자 주입할 때도 저절로 해주는 것
+ * 50 : 인자 3개중에 하나만 Matcher를 쓰지 못 한다. (Invalid use of argument matchers!)
+ * 57 : anyInt: any() null 값 허용
+ */
 @DisplayName("View 컨트롤러 - 게시글")
 @Import(SecurityConfig.class)
 @WebMvcTest(ArticleController.class)
@@ -30,12 +41,12 @@ class ArticleControllerTest {
 
     private final MockMvc mvc;
 
-    // Target: Type, Field 생성자 주입 안됨
     @MockBean
     private ArticleService articleService;
 
-    // 슬라이스 테스트에서는 생성자를 통한 주입이여도 @Autowired 해줘야함(WebMvcTest에 ExtendWith가 생성자 주입을 도움)
-    // 즉, spring 이 생성자 주입할 때도 저절로 해주는 것
+    @MockBean
+    private PaginationService paginationService;
+
     public ArticleControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
     }
@@ -44,8 +55,8 @@ class ArticleControllerTest {
     @Test
     public void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
         // Given
-        // 인자 3개중에 하나만 Matcher를 쓰지 못 한다. (Invalid use of argument matchers!)
         given(articleService.searchArticles(eq(null), eq(null), any(Pageable.class))).willReturn(Page.empty());
+        given(paginationService.getPaginationBarNumbers(anyInt(), anyInt())).willReturn(List.of(0, 1, 2, 3, 4));
 
         // When & Then
         mvc.perform(get("/articles"))
@@ -53,15 +64,49 @@ class ArticleControllerTest {
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("articles"))
-                .andExpect(model().attributeExists("searchTypes"));  // 어떤 searchType 존재하는 view에게 줘야함(프론트에서 작업을 해야하니까)
+                .andExpect(model().attributeExists("searchTypes"))  // 어떤 searchType 존재하는 view에게 줘야함(프론트에서 작업을 해야하니까)
+                .andExpect(model().attributeExists("paginationBarNumbers"));
         then(articleService).should().searchArticles(eq(null), eq(null), any(Pageable.class));
+        then(paginationService).should().getPaginationBarNumbers(anyInt(), anyInt());
+    }
+
+    @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 페이징, 정렬 정상 작동")
+    @Test
+    void givenPagingAndSortingParams_whenSearchingArticlesPage_thenReturnsArticlesPage() throws Exception {
+        // Given
+        String sortName = "title";
+        String direction = "desc";
+        int pageNumber = 0;
+        int pageSize = 5;
+        Pageable pageable = PageRequest.of(pageNumber, pageSize, Sort.by(Sort.Order.desc(sortName)));
+        List<Integer> barNumbers = List.of(1, 2, 3, 4, 5);
+
+        given(articleService.searchArticles(null, null, pageable)).willReturn(Page.empty());
+        given(paginationService.getPaginationBarNumbers(pageable.getPageNumber(), Page.empty().getTotalPages())).willReturn(barNumbers);
+
+        // When
+        mvc.perform(
+                        get("/articles")
+                                .queryParam("page", String.valueOf(pageNumber))
+                                .queryParam("size", String.valueOf(pageSize))
+                                .queryParam("sort", sortName + "," + direction)
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/index"))
+                .andExpect(model().attributeExists("articles"))
+                .andExpect(model().attribute("paginationBarNumbers", barNumbers));
+
+        // Then
+        then(articleService).should().searchArticles(null, null, pageable);
+        then(paginationService).should().getPaginationBarNumbers(pageable.getPageNumber(), Page.empty().getTotalPages());
     }
 
     @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
         // Given
-        Long articleId = 1L;
+        long articleId = 1L;
         given(articleService.getArticle(articleId)).willReturn(createArticleWithCommentsDto());
 
         // When & Then
