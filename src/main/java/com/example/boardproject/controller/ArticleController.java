4e12a0ee066015cf7aced5bdb380feeb768c9542
jmjmjames <chungjm0711@gmail.com>
2022-08-10 13:04:28 +0900

    #24 - 게시판 컨트롤러 구현 게시판 기본 조회 화면을 위한 컨트롤러를 구현하고 데이터를 전달

--- src/main/java/com/example/boardproject/controller/ArticleController.java
+++ src/main/java/com/example/boardproject/controller/ArticleController.java
@@ -1,28 +1,49 @@
 package com.example.boardproject.controller;
 
+import com.example.boardproject.domain.type.SearchType;
+import com.example.boardproject.dto.response.ArticleResponse;
+import com.example.boardproject.dto.response.ArticleWithCommentsResponse;
+import com.example.boardproject.service.ArticleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @RequestMapping("/articles")
 @Controller
 public class ArticleController {
 
+    private final ArticleService articleService;
+
     @GetMapping
-    public String articles(Model model) {
-        model.addAttribute("articles", List.of());
+    public String articles(
+            @RequestParam(required = false) SearchType searchType,
+            @RequestParam(required = false) String searchValue,
+            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
+            Model model
+    ) {
+        model.addAttribute("articles", articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from));
+        model.addAttribute("searchTypes", List.of());  // TODO: Enum에서 담아와야 함
 
         return "articles/index";
     }
+
     @GetMapping("/{articleId}")
     public String article(@PathVariable Long articleId, Model model) {
-        model.addAttribute("article", "article"); // TODO: 구현할 때 여기에 실제 데이터를 넣어줘야 한다
-        model.addAttribute("articleComments", List.of());
+        ArticleWithCommentsResponse article = ArticleWithCommentsResponse.from(articleService.getArticle(articleId));
+        model.addAttribute("article", article);
+        model.addAttribute("articleComments", article.articleCommentResponse());
 
         return "articles/detail";
     }
+
 }
--- src/test/java/com/example/boardproject/controller/ArticleControllerTest.java
+++ src/test/java/com/example/boardproject/controller/ArticleControllerTest.java
@@ -1,15 +1,25 @@
 package com.example.boardproject.controller;
 
 import com.example.boardproject.config.SecurityConfig;
+import com.example.boardproject.dto.ArticleWithCommentsDto;
+import com.example.boardproject.dto.UserDto;
+import com.example.boardproject.service.ArticleService;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import static org.mockito.BDDMockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -20,7 +30,11 @@ class ArticleControllerTest {
 
     private final MockMvc mvc;
 
-    // 슬라이스 테스트에서는 생성자를 통한 주입이여도 @Autowired 해줘야함
+    // Target: Type, Field 생성자 주입 안됨
+    @MockBean
+    private ArticleService articleService;
+
+    // 슬라이스 테스트에서는 생성자를 통한 주입이여도 @Autowired 해줘야함(WebMvcTest에 ExtendWith가 생성자 주입을 도움)
     // 즉, spring 이 생성자 주입할 때도 저절로 해주는 것
     public ArticleControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
@@ -30,27 +44,34 @@ class ArticleControllerTest {
     @Test
     public void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
         // Given
+        // 인자 3개중에 하나만 Matcher를 쓰지 못 한다. (Invalid use of argument matchers!)
+        given(articleService.searchArticles(eq(null), eq(null), any(Pageable.class))).willReturn(Page.empty());
 
         // When & Then
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/index"))
-                .andExpect(model().attributeExists("articles"));
+                .andExpect(model().attributeExists("articles"))
+                .andExpect(model().attributeExists("searchTypes"));  // 어떤 searchType 존재하는 view에게 줘야함(프론트에서 작업을 해야하니까)
+        then(articleService).should().searchArticles(eq(null), eq(null), any(Pageable.class));
     }
 
     @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
         // Given
+        Long articleId = 1L;
+        given(articleService.getArticle(articleId)).willReturn(createArticleWithCommentsDto());
 
         // When & Then
-        mvc.perform(get("/articles/1"))
+        mvc.perform(get("/articles/" + articleId))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
                 .andExpect(model().attributeExists("article"))
                 .andExpect(model().attributeExists("articleComments"));
+        then(articleService).should().getArticle(articleId);
     }
 
     @Disabled("구현 중")
@@ -77,6 +98,35 @@ class ArticleControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(model().attributeExists("articles/search-hashtag"));
+    }
+
+    private ArticleWithCommentsDto createArticleWithCommentsDto() {
+        return ArticleWithCommentsDto.of(
+                1L,
+                createUserDto(),
+                Set.of(),
+                "Title",
+                "content",
+                "#spring",
+                LocalDateTime.now(),
+                "jm",
+                LocalDateTime.now(),
+                "jm"
+        );
+    }
+
+    private UserDto createUserDto() {
+        return UserDto.of(
+                1L,
+                "jm@email.com",
+                "pwd",
+                "Jm",
+                "This is Memo",
+                LocalDateTime.now(),
+                "jm",
+                LocalDateTime.now(),
+                "jm"
+        );
     }
 
 }
