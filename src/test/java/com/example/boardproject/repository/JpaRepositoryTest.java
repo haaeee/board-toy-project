bb6362723ab44254c63f17e282868b84fadb5c80
Jongmin Chung <chungjm0711@gmail.com>
2022-08-11 13:09:50 +0900

    게시판 정렬 구현 (#37)
    * #31 - 게시판 뷰에 정렬 기능 업데이트
    정렬 기능은 spring data jpa + `Page` 이미 구현
    
    * #31 - 테스트 데이터 업데이트: 유저 추가
    article의 갯수를 변경 및 test 데이터에서 갯수 변경
    및 test email 수정

--- src/test/java/com/example/boardproject/repository/JpaRepositoryTest.java
+++ src/test/java/com/example/boardproject/repository/JpaRepositoryTest.java
@@ -46,7 +46,7 @@ class JpaRepositoryTest {
         // then
         assertThat(articles)
                 .isNotNull()
-                .hasSize(123);
+                .hasSize(117);
     }
 
     @DisplayName("insert_테스트")
@@ -54,7 +54,7 @@ class JpaRepositoryTest {
     void insert_test() {
         // given
         long previousCount = articleRepository.count();
-        User user = userRepository.save(User.of("jm@email.com", "pwd", null, null));
+        User user = userRepository.save(User.of("test@email.com", "pwd", null, null));
         Article article = Article.of(user, "new article", "new content", "#spring");
 
         // when
