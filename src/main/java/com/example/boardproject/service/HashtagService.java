b554543788d3780f1558017d4f71bc7e6ee9eda2
jmjmjames <chungjm0711@gmail.com>
2022-12-20 19:06:49 +0900

    #70 -  hashtag service 메서드 추가

--- src/main/java/com/example/boardproject/service/HashtagService.java
+++ src/main/java/com/example/boardproject/service/HashtagService.java
@@ -1,19 +1,55 @@
 package com.example.boardproject.service;
 
+import com.example.boardproject.domain.Hashtag;
+import com.example.boardproject.repository.HashtagRepository;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HashtagService {
 
-    public Object parseHashtagNames(String content) {
-        return null;
+    private final HashtagRepository hashtagRepository;
+
+    public HashtagService(final HashtagRepository hashtagRepository) {
+        this.hashtagRepository = hashtagRepository;
     }
 
-    public Object findHashtagsByNames(Set<String> expectedHashtagNames) {
-        return null;
+    public Set<String> parseHashtagNames(String content) {
+        if (Objects.isNull(content)) {
+            return Set.of();
+        }
+
+        Pattern pattern = Pattern.compile("#[\\w가-힣]+");
+        Matcher matcher = pattern.matcher(content.strip());
+
+        Set<String> result = new HashSet<>();
+
+        while (matcher.find()) {
+            result.add(matcher.group().replace("#", ""));
+        }
+
+        // return unmodifiable 보장하는 방법
+        return Set.copyOf(result);
     }
 
-    public void deleteHashtagWithoutArticles(Object any) {
+    public Set<Hashtag> findHashtagsByNames(Set<String> expectedHashtagNames) {
+        return new HashSet<>(hashtagRepository.findByHashtagNameIn(expectedHashtagNames));
+    }
+
+    public void deleteHashtagWithoutArticles(Long hashtagId) {
+        Hashtag hashtag = hashtagRepository.getReferenceById(hashtagId);
+
+        if (hashtag.getArticles().isEmpty()) {
+            hashtagRepository.delete(hashtag);
+        }
+    }
+
+    public List<String> findAllHashtagNames() {
+        return hashtagRepository.findAllHashtagNames();
     }
 }
