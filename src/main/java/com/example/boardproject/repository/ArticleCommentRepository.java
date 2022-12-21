5d3fd3503beae81ea3dee4493e7b3b3a222043b6
jmjmjames <chungjm0711@gmail.com>
2022-12-21 12:13:34 +0900

    #76 - fetchType LAZY 변경에 따른 쿼리 작성

--- src/main/java/com/example/boardproject/repository/ArticleCommentRepository.java
+++ src/main/java/com/example/boardproject/repository/ArticleCommentRepository.java
@@ -4,13 +4,19 @@ import com.example.boardproject.domain.ArticleComment;
 import com.example.boardproject.domain.QArticleComment;
 import com.querydsl.core.types.dsl.DateTimeExpression;
 import com.querydsl.core.types.dsl.StringExpression;
+import java.util.Collection;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
 import org.springframework.data.querydsl.binding.QuerydslBindings;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
+import org.springframework.transaction.annotation.Transactional;
 
 @RepositoryRestResource
 public interface ArticleCommentRepository extends
@@ -19,9 +25,18 @@ public interface ArticleCommentRepository extends
         QuerydslBinderCustomizer<QArticleComment> {
 
     // Article Repo 의(_) Id
-    List<ArticleComment> findByArticle_Id(Long articleId);
+    @Query("select ac from ArticleComment ac join fetch ac.user join fetch ac.childComments where ac.article.id = :articleId")
+    List<ArticleComment> findWithUserByArticle_Id(@Param("articleId") Long articleId);
+
+    @Query("select ac from ArticleComment ac join fetch ac.user join fetch ac.childComments where ac.id = :id")
+    Optional<ArticleComment> findWithUserAndChildCommentsById(@Param("id") Long id);
 
     void deleteByIdAndUser_Email(Long articleCommentId, String email);
+
+    @Transactional
+    @Modifying(clearAutomatically = true)
+    @Query("delete from ArticleComment ac where ac.id in :childIds")
+    void deleteByChildCommentId(@Param("childIds") Collection<Long> childIds);
 
     @Override
     default void customize(QuerydslBindings bindings, QArticleComment root) {
