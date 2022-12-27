c57548f66ba54d159c469851db72584a726cccce
jmjmjames <chungjm0711@gmail.com>
2022-12-27 18:01:17 +0900

    #85 - 인증 설정 업데이트: OAuth2 인증 설정 추가
    스프링 시큐리티가 기존 DB 인증에다가 OAuth 인증까지 받아들일 수
    있도록 추가

--- src/main/java/com/example/boardproject/config/SecurityConfig.java
+++ src/main/java/com/example/boardproject/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.example.boardproject.config;
 
-import com.example.boardproject.dto.UserDto;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import com.example.boardproject.dto.security.KakaoOAuth2Response;
 import com.example.boardproject.dto.security.UserPrincipal;
-import com.example.boardproject.repository.UserRepository;
+import com.example.boardproject.service.UserService;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +14,10 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 
 //@EnableWebSecurity  // 넣지 않아도 된다. -> Spring Boot AutoConfig 에 들어감.
@@ -19,10 +25,14 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http,
+            OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService) throws Exception {
+
         return http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()  // StaticResourceLocation {css, js}
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+                        .permitAll()  // StaticResourceLocation {css, js}
                         .mvcMatchers(
                                 HttpMethod.GET,
                                 "/",
@@ -31,21 +41,46 @@ public class SecurityConfig {
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
-                .formLogin()
-                        .and()
-                .logout()
-                        .logoutSuccessUrl("/")
-                        .and()
+                .formLogin(withDefaults())
+                .logout(logout -> logout.logoutSuccessUrl("/"))
+                .oauth2Login(oAuth -> oAuth
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .userService(oAuth2UserService)
+                        )
+                )
                 .build();
     }
 
     @Bean
-    public UserDetailsService userDetailsService(UserRepository userRepository) {
-        return username -> userRepository
-                .findByEmail(username)
-                .map(UserDto::from)
+    public UserDetailsService userDetailsService(UserService userService) {
+        return username -> userService
+                .searchUser(username)
                 .map(UserPrincipal::from)
                 .orElseThrow(() -> new UsernameNotFoundException("유저를 찾을 수 없습니다. - userName: " + username));
+    }
+
+    @Bean
+    public OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService(
+            final UserService userService,
+            final PasswordEncoder passwordEncoder) {
+
+        final DefaultOAuth2UserService delegate = new DefaultOAuth2UserService();
+
+        return userRequest -> {
+            OAuth2User oAuth2User = delegate.loadUser(userRequest);
+
+            KakaoOAuth2Response kakaoOAuth2Response = KakaoOAuth2Response.from(oAuth2User.getAttributes());
+//            String registrationId = userRequest.getClientRegistration().getRegistrationId();  // "kakao"
+//            String providerId = String.valueOf(kakaoOAuth2Response.id());
+            String username = kakaoOAuth2Response.email();
+            String dummyPassword = passwordEncoder.encode("{bcrypt}dummy");
+
+            return userService.searchUser(username)
+                    .map(UserPrincipal::from)
+                    .orElseGet(() -> UserPrincipal.from(
+                            userService.saveUser(username, dummyPassword, kakaoOAuth2Response.nickname(),
+                                    null)));
+        };
     }
 
     @Bean
