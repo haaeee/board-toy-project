b7eec4a0bdb2d918f10409ec031e698f10a0c91a
jmjmjames <chungjm0711@gmail.com>
2022-12-27 14:53:13 +0900

    #86 - private protected로 변경

--- src/main/java/com/example/boardproject/domain/AuditingFields.java
+++ src/main/java/com/example/boardproject/domain/AuditingFields.java
@@ -18,22 +18,24 @@ import org.springframework.format.annotation.DateTimeFormat;
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 public abstract class AuditingFields {
+
     // 상식적으로 Jpa Auditing 은 not null
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     @CreatedDate
     @Column(nullable = false, updatable = false)
-    private LocalDateTime createdAt;  // 생성일시
+    protected LocalDateTime createdAt;  // 생성일시
 
     @CreatedBy
     @Column(nullable = false, updatable = false, length = 100)
-    private String createdBy;  // 생성자
+    protected String createdBy;  // 생성자
 
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     @LastModifiedDate
     @Column(nullable = false)
-    private LocalDateTime modifiedAt;  // 수정일시
+    protected LocalDateTime modifiedAt;  // 수정일시
 
     @LastModifiedBy
     @Column(nullable = false)
-    private String modifiedBy;  // 수정자
+    protected String modifiedBy;  // 수정자
+
 }
